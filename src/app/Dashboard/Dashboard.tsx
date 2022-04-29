633beee5a4f17774bd72c5cfa4127c477dc7b5ff
Mohammadi Iram <mohammadiiram@gmail.com>
2022-04-29 11:19:42 +0530

    todo

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,10 +1,40 @@
 import * as React from 'react';
 import { PageSection, Title } from '@patternfly/react-core';
 
-const Dashboard: React.FunctionComponent = () => (
+const Dashboard: React.FunctionComponent = () => {
+
+  const [currentInput, setCurrentInput] = React.useState('')
+  const [list, setList] = React.useState([]);
+
+  const AddTodo = (event) => {
+    event.preventDefault();
+    // const newList = list;
+    // newList.push(currentInput);
+    setList([...list, currentInput]);
+    console.log(list);
+  }
+  return (
   <PageSection>
     <Title headingLevel="h1" size="lg">Dashboard Page Title!</Title>
-  </PageSection>
-)
+    <h1>TODO APP</h1>
+<form className="input-box">
+<input className="input" placeholder="enter you name" onChange={(event)=>{
+  setCurrentInput(event.target.value)
+}}/>
 
+
+<button className="add-button" onClick={AddTodo}>Add</button>
+
+
+</form>
+{list.map((singleTodo)=>{
+  return<div>{singleTodo}</div>
+})}
+
+
+
+  </PageSection>
+
+)
+}
 export { Dashboard };
