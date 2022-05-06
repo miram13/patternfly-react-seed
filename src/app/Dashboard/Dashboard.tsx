fe3a89248f5b8429816ae69d0236ee0321c90636
Mohammadi Iram <mohammadiiram@gmail.com>
2022-05-06 22:41:16 +0530

    patternfly_table

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -6,54 +6,44 @@ import axios from 'axios';
 import { PageSection } from '@patternfly/react-core';
 
 const Dashboard: React.FunctionComponent = () => {
-const [lists, setLists] = React.useState([])
+  const [lists, setLists] = React.useState([]);
 
-const listtable = {
-  name: 'name',
-  id: 'id'
-};
-
+  const listtable = {
+    name: 'name',
+    id: 'id',
+  };
 
   React.useEffect(() => {
     const getUsers = async () => {
-        const response = await axios.get(`https://api.github.com/users`);
-        setLists(response.data);
+      const response = await axios.get(`https://api.github.com/users`);
+      setLists(response.data);
     };
     getUsers();
-    
   }, []);
-console.log(lists)
-return(
-  <PageSection>
-    <TableComposable
-        aria-label="Simple table"
-      
-      >
-      <Caption>Table</Caption>
-      <Thead>
-      
-            <Tr>
+  console.log(lists);
+  return (
+    <PageSection>
+      <TableComposable aria-label="Simple table">
+        <Caption>Table</Caption>
+        <Thead>
+          <Tr>
             <Th>{listtable.name}</Th>
             <Th>{listtable.id}</Th>
-            </Tr>
-            
-      </Thead>
-        
-      <Tbody>
-          {lists.map(repo => {
-            return(
-              <Tr>
-              <Td dataLabel={listtable.name}>{repo.login}</Td>
-              <Td dataLabel={listtable.id}>{repo.id}</Td>
-            </Tr>
-            )
-            
-})}
-        </Tbody>
-         
-</TableComposable>
-  </PageSection>
-)
+          </Tr>
+        </Thead>
 
+        <Tbody>
+          {lists.map((repo) => {
+            return (
+              <Tr>
+                <Td dataLabel={listtable.name}>{repo.login}</Td>
+                <Td dataLabel={listtable.id}>{repo.id}</Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </TableComposable>
+    </PageSection>
+  );
 };
 export { Dashboard };
