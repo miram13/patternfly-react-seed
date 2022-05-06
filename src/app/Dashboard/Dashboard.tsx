12ce367d58f036a7aa472617be9fe818b316bc8c
Mohammadi Iram <mohammadiiram@gmail.com>
2022-05-06 21:03:23 +0530

    table

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,10 +1,59 @@
 import * as React from 'react';
-import { PageSection, Title } from '@patternfly/react-core';
 
-const Dashboard: React.FunctionComponent = () => (
+import { TableComposable, Caption, Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
+import { string } from 'prop-types';
+import axios from 'axios';
+import { PageSection } from '@patternfly/react-core';
+
+const Dashboard: React.FunctionComponent = () => {
+const [lists, setLists] = React.useState([])
+
+const listtable = {
+  name: 'name',
+  id: 'id'
+};
+
+
+  React.useEffect(() => {
+    const getUsers = async () => {
+        const response = await axios.get(`https://api.github.com/users`);
+        setLists(response.data);
+    };
+    getUsers();
+    
+  }, []);
+console.log(lists)
+return(
   <PageSection>
-    <Title headingLevel="h1" size="lg">Dashboard Page Title!</Title>
+    <TableComposable
+        aria-label="Simple table"
+      
+      >
+      <Caption>Table</Caption>
+      <Thead>
+      
+            <Tr>
+            <Th>{listtable.name}</Th>
+            <Th>{listtable.id}</Th>
+            </Tr>
+            
+      </Thead>
+        
+      <Tbody>
+          {lists.map(repo => {
+            return(
+              <Tr>
+              <Td dataLabel={listtable.name}>{repo.login}</Td>
+              <Td dataLabel={listtable.id}>{repo.id}</Td>
+            </Tr>
+            )
+            
+})}
+        </Tbody>
+         
+</TableComposable>
   </PageSection>
 )
 
+};
 export { Dashboard };
